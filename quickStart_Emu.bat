1a2144a7a427919e19c2087fc6994979ce0746c2
ligeek <ligeek@outlook.com>
2021-01-07 10:06:51 +0800

    Added 奇景明信片

--- quickStart_Emu.bat
+++ quickStart_Emu.bat
@@ -3,7 +3,12 @@
 
 :begin
 set /p slim="输入预期战斗次数: "
-if %slim% == r  (.\ADB\win32\adb.exe connect 127.0.0.1:7555) else (python akhelper.py quick %slim%)
+if %slim% == r (goto rst) else (python akhelper.py quick %slim%)
+goto begin
+
+:rst
+taskkill /f /im adb.exe
+.\ADB\win32\adb.exe connect 127.0.0.1:7555
 goto begin
 REM set /p choose="是否还要继续？（Y/N）："
 REM if %choose% == Y goto begin
